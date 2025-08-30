f3046c6507039e671a462ec6414ef325ffda64e6
Karthika Bala <bkarthika1203@gmail.com>
2025-08-30 12:17:40 +0530

    Add entire DSA_PREP folder

---
+++ README.md
@@ -0,0 +1,14 @@
+# 📘 Data Structures in C – My Coding Journey  
+
+This repository is an account of my coding journey in **Data Structures using C language**.  
+By the time I sit for placements in **2028**, I hope to:  
+- Solidify my understanding of Data Structures and Algorithms.  
+- Solve a large number of problems in **increasing order of difficulty**.  
+- Build the problem-solving mindset needed for real-world applications.  
+
+---
+
+## 🚀 Goals  
+- Daily practice and consistency.  
+- Clear, well-structured solutions with comments.  
+- Tracking progress over the years until placements.
---
+++ stacks/bracket_checker.c
@@ -0,0 +1,70 @@
+/*Equality Checker for brackets. 
+Write a program to check whether the brackets in a string are balanced using stacks.  
+Example:  
+- Input: `"{[()]}"` → Output: `Balanced`  
+- Input: `"{[(])}"` → Output: `Not Balanced`  */
+#include<stdio.h>
+#include<string.h>
+#define MAX 100
+char stack[MAX];
+int top=-1;
+void push(char val){
+    if(top!=MAX-1){
+        stack[++top]=val;
+    }
+}
+char pop(){
+    if(top!=-1){
+        return stack[top--];
+    }
+}
+char peek(){
+    return stack[top];
+}
+int isFull(){
+    return top==MAX;
+}
+int isEmpty(){
+    return top==-1;
+}
+bool open(char val){
+    return(val=='('||val=='['||val=='{');
+}
+bool close(char val){
+    return(val==')'||val==']'||val=='}');
+}
+bool isMatch(char open,char close){
+    return(open=='('&&close==')');
+    return(open=='['&&close==']');
+    return(open=='{'&&close=='}');
+}
+bool balanced(char str[]){
+    int n=strlen(str);
+    for(int i=0;i<n;i++){
+        if(open(str[i])){
+            push(str[i]);
+        }
+        else if(close(str[i])){
+            if(isEmpty()){
+                return false;
+            }
+            if(!isMatch(pop(),str[i])){
+                return false;
+            }
+        }
+    }
+    return isEmpty();
+}
+int main(){
+    char str[100];
+    printf("enter string: ");
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+
+    if(balanced(str)){
+        printf("brackets are balanced");
+    }
+    else{
+        printf("not balanced");
+    }
+}
